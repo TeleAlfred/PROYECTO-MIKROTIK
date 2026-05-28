93153ab51393ae47582e71495d06a24213d23111
alejandrofc2709 <1005.31467299.ucla@gmail.com>
2026-05-28 07:59:08 -0400

    Subida de trabajo de frontend

---
+++ appMikrotik/apps/core/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from pyBCV import Currency
+
+# Funcion para obtener la tasa de cambio actual
+def obtener_tasa_actual():
+        try:
+            tasa = Currency().get_rate('USD',prettify=False)
+            return round(float(tasa), 2)
+        except Exception as e:
+            print(f"Error al obtener la tasa de cambio: {e}")
+            return None
+
+# Create your models here.
+class Plan(models.Model):
+    plan = models.CharField(max_length=20)
+    precioUSD = models.DecimalField(max_digits=10, decimal_places=2)
+    velocidad_subida = models.DecimalField(max_digits=10, decimal_places=2)
+    velocidad_bajada = models.DecimalField(max_digits=10, decimal_places=2)
+    borrado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.plan} | {self.precioUSD}$ | {self.velocidad_subida}MB | {self.velocidad_bajada}MB"
+    
+class Cliente(models.Model):
+    class SeleccionEstado(models.TextChoices):
+        SOLVENTE = 'Solvente', 'Solvente'
+        EXONERADO = 'Exonerado', 'Exonerado'
+        PENDIENTE = 'Pendiente', 'Pendiente'
+        DESCONECTADO = 'Desconectado', 'Desconectado'  
+
+    idPlan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=9, unique=True)
+    celular = models.CharField(max_length=12)
+    direccion = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150, unique=True)
+    direccionIP = models.GenericIPAddressField(protocol='IPv4', unique=True)
+    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    estado = models.CharField(max_length=20, choices=SeleccionEstado.choices, default=SeleccionEstado.SOLVENTE)
+    borrado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.cedula} | {self.nombre} | {self.celular} | {self.email} | {self.saldo}$ | {self.estado}"
+    
+class Factura(models.Model):
+    idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    montoUSD = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.idCliente} | {self.montoUSD}$ | {self.fecha}"
+
+class Pago(models.Model):
+    class SeleccionMetodo(models.TextChoices):
+        TRANSFERENCIA = 'Transferencia', 'Transferencia'
+        PAGO_MOVIL = 'Pago Móvil', 'Pago Móvil'
+        ZELLE = 'Zelle', 'Zelle'
+        EFECTIVOBS = 'Efectivo Bs', 'Efectivo Bs'
+        EFECTIVODOLARES = 'Efectivo $', 'Efectivo $'
+
+    idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    idPersonal = models.ForeignKey(User, on_delete=models.CASCADE)
+    montoUSD = models.DecimalField(max_digits=10, decimal_places=2)
+    tasa = models.DecimalField(max_digits=10, decimal_places=2, default=obtener_tasa_actual)
+    metodo = models.CharField(max_length=20, choices=SeleccionMetodo.choices)
+    fecha = models.DateTimeField(default=timezone.now)
+    comprobante = models.ImageField(upload_to='comprobantes/%Y/%m/%d/',null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.idCliente.cedula} {self.idCliente.nombre} | {self.idPersonal.username} | {self.montoUSD}$ | {self.fecha}"
+    
+    @property
+    def monto_bs(self):
+        return round(self.montoUSD * self.tasa,2)
+    
+class Logs(models.Model):
+    idPersonal = models.ForeignKey(User, on_delete=models.CASCADE)
+    modulo = models.CharField(max_length=30)
+    mensaje = models.TextField()
+    error = models.BooleanField(default=False)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.idPersonal.username} | {self.modulo} | {self.fecha}"
---
+++ appMikrotik/apps/core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('login')
+
+def signin(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+        else:
+            print('sapo')
+            render(request, 'login.html', {'form':form}, {'error':'Formulario no valido'})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
