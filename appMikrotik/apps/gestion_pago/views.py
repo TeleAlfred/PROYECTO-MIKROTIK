d7c1d6b9e8155ef57db0db95a7a391ebf6c00be2
Dante Dacosta <dantedacosta2001@gmail.com>
2026-05-28 23:33:04 -0400

    Implementacion del back para mostrar Logs (#27)
    * Implementacion del backEnd para mostrar Logs
    
    * Ordenar los logs por fecha
    
    * Hacer los campos del filtro no obligatorios

--- appMikrotik/apps/gestion_pago/views.py
+++ appMikrotik/apps/gestion_pago/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from core.models import Pago, Logs, Cliente
 from .forms import PagoForm, FiltroPagos
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def gestion_pago(request, id):
     pagos = Pago.objects.all().order_by('-fecha')
 
@@ -13,8 +16,6 @@ def gestion_pago(request, id):
             nombreCliente = filtro.cleaned_data.get('nombreCliente')
             fecha_inicio = filtro.cleaned_data.get('fecha_inicio')
             fecha_fin = filtro.cleaned_data.get('fecha_fin')
-
-            print(f"Nombre del cliente: {nombreCliente}, Fecha inicio: {fecha_inicio}, Fecha fin: {fecha_fin}") 
 
             if nombreCliente:
                 pagos = pagos.filter(idCliente__nombre__icontains=nombreCliente)
@@ -27,7 +28,6 @@ def gestion_pago(request, id):
                 pagos = pagos.filter(fecha__lte=fecha_fin)
 
             pagos = pagos.order_by('-fecha')
-            return render(request, 'gestion_pagos.html', {'pagos': pagos, 'filtros': filtro})
     else:
         filtro = FiltroPagos()
         if id != 0:
@@ -35,7 +35,7 @@ def gestion_pago(request, id):
         
     return render(request, 'gestion_pagos.html', {'pagos': pagos, 'filtros': filtro})
 
-
+@login_required
 def crear_pago(request):
     if request.method == 'POST':
         form = PagoForm(request.POST, request.FILES)
@@ -86,6 +86,7 @@ def crear_pago(request):
         form = PagoForm()
     return render(request, 'crear_pago.html', {'form': form})
 
+@login_required
 def modificar_pago(request, id):
     pago = get_object_or_404(Pago, id=id)
 
@@ -179,6 +180,7 @@ def modificar_pago(request, id):
         
     return render(request, 'modificar_pago.html', {'form': form})
 
+@login_required
 def mostrar_detalles(request, id):
     pago = get_object_or_404(Pago, id=id)
     cliente = pago.idCliente
